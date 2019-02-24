2a695ad327532cf1a88f31349ef1665455fd4908
Peter Tag <peter.h.tag@gmail.com>
2019-02-24 11:59:51 -0800

    updates...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,44 @@
-window.onscroll = function() {myFunction()};
-// var navbar = document.getElementById("navbar");
-// alert($(".navbar").offset().top);
-var navbar = $(".navbar");
-var sticky = navbar.offset().top;
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-    navbar.addClass("sticky")
-    navbar.css({'overflow':'visible'});
-    } else {
-    navbar.removeClass("sticky");
-    navbar.css({'overflow':'hidden'});
-    }
+var internalScrollingEvent = false;
+var scrollToPosition = {
+        left: 0,
+        top: 0
 }
 
+$(document).ready(function(){
+    // $(this).scrollTop(0);
+    // $(window).scrollTop();  // Frustratingly, this doesn't work. gave up after several hours of trying & testing different things
+    window.scrollTo(0, 500);
+    window.onscroll = function() {myFunction()};
+    // window.onbeforeunload = function () {
+    //     window.scrollTo(0, 0);
+    //   }
+    var navbar = $(".navbar");
+    var sticky = navbar.offset().top;
+    var about = $("#grid-about").offset().top;
+    var contact = $("#grid-contact").offset().top;
+    var portfolio = $("#grid-portfolio").offset().top;
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.addClass("sticky")
+            navbar.css({'overflow':'visible'});
+        } else {
+            navbar.removeClass("sticky");
+            navbar.css({'overflow':'hidden'});
+        }
+        if (internalScrollingEvent) {
+            window.scrollTo(0, scrollToPosition.top -100);  // subtract 100 to deal with disappearing header
+            internalScrollingEvent=false;   // turn this off or else we will infinitely loop
+        }
+    }
+    $(document).on('click', '.navbar', function(event) {
+        // build the id string that will be the target of the scroll action
+        var myElement = '#grid-' + $(event.target).attr('id');
+        scrollToPosition.left = 0;
+        scrollToPosition.top = $(myElement).position().top
+        internalScrollingEvent=true;
+        // Crazily, this doesn't move to the specified target because it triggers a scroll 
+        // function, above. So we actually end up doing the scrolling action in the scroll 
+        // response function
+        window.scrollTo(0, scrollToPosition.top);   
+    });
+});
